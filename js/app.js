fb857bf602c74bcecf2470197850ac8847ba704d
Cordelle <cordellec.charbonneau@hotmail.com>
2023-02-22 14:24:14 -0700

    I am uploading my work to Github

--- js/app.js
+++ js/app.js
@@ -28,11 +28,56 @@ const handleSubmit = function(event) {
   // Using if, else if and else blocks: assign a meal recommendation to `answer` based on the submitted options for `day` and `meal`.
 
   // For example: 
-  if (day === 'tue') {
-    answer = 'tacos';
-  }
 
 
+// I just used an "If" statement at first, and than everything else was "else if" statments sepearated by day and meal, with diffrent meal pretaining to that time and day//
+// I used the "else" statement at the end of the "If" & "Else If" statmenet's for a genral fall back for any of the statmenets if they do fail to provide a food //
+
+ if (day === "mon" && meal === "breakfast") {
+  answer = "Egg Burger";
+ } else if (day === "mon" && meal === "lunch") {
+  answer = "Veggie Burger";
+ } else if ( day === "mon" && meal === "dinner") {
+  answer = "Beef Burger";
+ } else if (day==="tue" && meal === "breakfast"){
+  answer = "Egg Taco's";
+} else if (day === "tue" && meal === "lunch"){
+  answer = "Veggie Taco's";
+} else if (day === "tue" && meal === "dinner"){
+  answer = "Beef Taco's";
+} else if (day === "wed" && meal === "breakfast") {
+  answer = "Egg Wings";
+} else if (day === "wed" && meal === "lunch") {
+  answer = "Veggie Wings";
+} else if (day === "wed" && meal === "dinner"){
+  answer = "Beef Wings";
+}  else if (day === "thu" && meal === "breakfast") {
+  answer = "Egg Pho";
+} else if (day === "thu" && meal === "lunch") {
+  answer = "Veggie Pho";
+} else if (day === "thu" && meal === "dinner") {
+  answer = "Beef Pho";
+}  else if (day === "fri" && meal === "breakfast"){
+  answer = "Egg Pizza";
+} else if (day === "fri" && meal === "lunch"){
+  answer = "Veggie Pizza";
+} else if (day === "fri" && meal === "dinner"){
+  answer = "Beef Pizza";
+} else if (day === "sat" && meal === "breakfast") {
+  answer = "Egg Nacho's";
+} else if (day === "sat" && meal === "lunch") {
+  answer = "Veggie Nacho's";
+} else if (day === "sat" && meal === "dinner") {
+  answer = "Beef Nacho's";
+} else if (day === "sun" && meal === "breakfast") {
+  answer = "Egg Potachos";
+} else if (day === "sun" && meal === "lunch") {
+  answer = "Veggie Potachos";
+} else if (day === "sun" && meal === "dinner") {
+  answer = "Beef Potachos";
+} else {
+  answer = "No food :("
+}
 
   /************************************/
   /* Don't edit the rest of this code */
